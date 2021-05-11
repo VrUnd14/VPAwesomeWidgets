8d1acfaa5acae951e84778bd01140fed0e9ae8f1
Vrund Purohit <vrund14@gmail.com>
2021-05-11 15:58:38 +0530

    Bug Fix VPIconTextView

--- VPAwesomeWidgets/src/main/java/awesome/vrund/vpawesomewidgets/VPEditText.kt
+++ VPAwesomeWidgets/src/main/java/awesome/vrund/vpawesomewidgets/VPEditText.kt
@@ -3,19 +3,42 @@ package awesome.vrund.vpawesomewidgets
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.text.InputFilter
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.graphics.ColorUtils
 
-class VPEditText @JvmOverloads constructor( context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.editTextStyle) : AppCompatEditText(context, attrs, defStyleAttr) {
+class VPEditText @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.editTextStyle) : AppCompatEditText(context, attrs, defStyleAttr) {
 
     private val mContext = context
 
     private var backColor = 0xFFF1F1F1.toInt()
+        set(value) {
+            field = value
+            updateUI()
+        }
     private var hasBorder = true
+        set(value) {
+            field = value
+            updateUI()
+        }
 
     private var borderColor = 0xFFc3c3c3.toInt()
-    private var enable = true
+        set(value) {
+            field = value
+            updateUI()
+        }
+    private var isEnable = true
+        set(value) {
+            field = value
+            updateUI()
+        }
+
+    private var textAllCaps = true
+        set(value) {
+            field = value
+            updateUI()
+        }
 
     init {
         this.setBackgroundResource(R.drawable.vp_auto_back)
@@ -25,46 +48,26 @@ class VPEditText @JvmOverloads constructor( context: Context, attrs: AttributeSe
         hasBorder = parent.getBoolean(R.styleable.VPEditText_et_hasBorder, hasBorder)
 
         borderColor = parent.getColor(R.styleable.VPEditText_et_borderColor, borderColor)
-        enable = parent.getBoolean(R.styleable.VPEditText_et_enable, enable)
+        isEnable = parent.getBoolean(R.styleable.VPEditText_et_enable, isEnabled)
+        textAllCaps = parent.getBoolean(R.styleable.VPAutoCompleteTextView_act_textAllCaps, textAllCaps)
 
         parent.recycle()
-
-        updateUI()
     }
 
     private fun updateUI() {
-        val tinColor = borderColor.takeIf { enable } ?: ColorUtils.blendARGB(borderColor, Color.WHITE,0.6f)
+        val tinColor = borderColor.takeIf { isEnabled }
+                ?: ColorUtils.blendARGB(borderColor, Color.WHITE, 0.6f)
 
         // Main
         val mainGD = this.background as GradientDrawable
         mainGD.setColor(backColor)
-        if (hasBorder)
-            mainGD.setStroke(1, tinColor)
-        else
-            mainGD.setStroke(0, tinColor)
+        mainGD.setStroke(1.takeIf { hasBorder } ?: 0, tinColor)
         this.background = mainGD
-        this.isEnabled = enable
-    }
+        this.isEnabled = isEnabled
 
-    fun setBackColor(color: Int) {
-        backColor = color
-        updateUI()
-    }
-
-    fun getBackColor(): Int {
-        return backColor
-    }
-
-    fun setBoarderColor(color: Int) {
-        borderColor = color
-        updateUI()
-    }
-
-    fun getBorderColor(): Int {
-        return borderColor
-    }
-
-    fun isEnable(): Boolean {
-        return enable
+        if (textAllCaps)
+            this.filters = arrayOf(InputFilter.AllCaps())
+        else
+            this.filters = arrayOf()
     }
 }
