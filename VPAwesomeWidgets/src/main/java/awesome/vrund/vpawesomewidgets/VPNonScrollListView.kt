5a3dff54910fda2e91d2d0c18551a8b2ed9651c3
Vrund Purohit <vrund14@gmail.com>
2021-06-11 17:50:09 +0530

    Added VPNonScrollGridView & VPNonScrollListView

---
+++ VPAwesomeWidgets/src/main/java/awesome/vrund/vpawesomewidgets/VPNonScrollListView.kt
@@ -0,0 +1,22 @@
+package awesome.vrund.vpawesomewidgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ListView
+
+class VPNonScrollListView : ListView {
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {}
+
+    public override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val heightMeasureSpec_custom = View.MeasureSpec.makeMeasureSpec(
+            Integer.MAX_VALUE shr 2, View.MeasureSpec.AT_MOST)
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec_custom)
+        val params = layoutParams
+        params.height = measuredHeight
+    }
+}
